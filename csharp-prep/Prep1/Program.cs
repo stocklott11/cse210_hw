71df9cc523c13ff96dd1f96e3749e7df3b301fa7
stocklott11 <stocklott11@gmail.com>
2024-11-01 13:29:17 -0600

    Name assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
-}
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello Prep1 World!");
+//     }
+// }
+Console.Write("What is your first name? ");
+string first_name = Console.ReadLine();
+
+Console.Write("What is your last name? ");
+string last_name = Console.ReadLine();
+
+Console.WriteLine($"You name is {last_name}, {first_name} {last_name}.");
