0eef85a15003562989b46931e98099a2488dfdf4
stocklott11 <stocklott11@gmail.com>
2024-11-01 15:25:07 -0600

    Random Number assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,34 @@
 using System;
 
-class Program
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello Prep3 World!");
+//     }
+// }
+Random randomGenerator = new Random();
+int magic_number = randomGenerator.Next(1, 101);
+
+int guess = -1;
+
+while (guess != magic_number)
 {
-    static void Main(string[] args)
+    Console.Write("What is your guess? ");
+    guess = int.Parse(Console.ReadLine());
+    
+    if (magic_number > guess)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Higher");
     }
-}
+    else if (magic_number < guess)
+    {
+        Console.WriteLine("Lower");
+    }
+    else
+    {
+        Console.WriteLine("Good job! You guessed it!");
+    }
+} 
+
+
