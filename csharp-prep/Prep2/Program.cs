8d97935549bd764195cc7e4df2104d81181d93d9
stocklott11 <stocklott11@gmail.com>
2024-11-01 13:46:56 -0600

    Grades assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Data;
 
-class Program
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello Prep2 World!");
+//     }
+// }
+
+Console.Write("What is your grade percentage? ");
+string percentage = Console.ReadLine();
+int percent = int.Parse(percentage);
+
+string letter_grade = "";
+
+if (percent >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+    letter_grade = "A";
 }
+else if (percent >= 80)
+{
+    letter_grade = "B";
+}
+else if (percent >= 70)
+{
+    letter_grade = "C";
+}
+else if (percent >= 60)
+{
+    letter_grade = "D";
+}
+else 
+{
+    letter_grade = "F";
+}
+
+Console.WriteLine($"Your letter grade is {letter_grade}");
