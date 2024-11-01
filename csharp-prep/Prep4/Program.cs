e2c95c155cdbb44f4641c9913d612dcc8dbd1ad0
stocklott11 <stocklott11@gmail.com>
2024-11-01 15:38:09 -0600

    Lists assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello Prep4 World!");
+//     }
+// }
+
+List<int> numbers = new List<int>();
+
+    int user_number = -1;
+    while (user_number != 0)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.Write("Enter a number (Press 0 to quit): ");
+            
+        string userResponse = Console.ReadLine();
+        user_number = int.Parse(userResponse);
+
+        if (user_number != 0)
+        {
+            numbers.Add(user_number);
+        }
     }
-}
+
+    int sum = 0;
+    foreach (int number in numbers)
+    {
+        sum += number;
+    }
+
+    Console.WriteLine($"The sum is: {sum}");
+
+    float average = ((float)sum) / numbers.Count;
+    Console.WriteLine($"The average is: {average}");
+
+    int max = numbers[0];
+
+    foreach (int number in numbers)
+    {
+        if (number > max)
+        {
+            max = number;
+        }
+    }
+
+    Console.WriteLine($"The max is: {max}");
