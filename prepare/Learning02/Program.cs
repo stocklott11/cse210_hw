0e040c749f22597b000ba0db1b38feb7f7f46e3e
stocklott11 <stocklott11@gmail.com>
2024-11-04 20:03:55 -0700

    Week 2 Prepare Assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Salesman";
+        job1._company = "Taylor Chevrolet";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Liberty GMC";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "John Doe";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
